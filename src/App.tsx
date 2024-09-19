3661ec49812462f6f2711cf93f8eb80d9e915797
Michele Barbiero <barbmich.since86@gmail.com>
2024-09-19 18:23:55 -0400

    stuff to test browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,35 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  useMutation,
-  useQuery,
-  useQueryClient,
-} from "@tanstack/react-query";
-import "./App.css";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Link, Route, Routes } from "react-router-dom";
 
-let todoCounter = 1;
-let todos = [{ id: 1, text: "Buy milk" }];
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
 
-function simulateAsync(res: any) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(res);
-    }, 1000);
-  });
-}
-
-const queryClient = new QueryClient();
-function App() {
+export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryTest />
-      <ReactQueryDevtools initialIsOpen={true} />
+      <nav>
+        <Link to="/">root</Link>
+        {" | "}
+        <Link to="/about">about</Link>
+        {" | "}
+        <Link to="/contact">contact</Link>
+        {" | "}
+        <Link to="../inexistent-page">inexistent page</Link>
+        {" | "}
+        <Link to="../../other/basename/path">other basename path</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>root page</div>} />
+        <Route path="/about" element={<div>about page</div>} />
+        <Route path="/contact" element={<div>contact page</div>} />
+      </Routes>
+      <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
-  );
-}
-
-export default App;
-
-function ReactQueryTest() {
-  const [state, setState] = useState("");
-  const [num, setNum] = useState("");
-
-  const queryClient = useQueryClient();
-
-  console.log(queryClient.getQueryData(["todos"]));
-
-  const query = useQuery({
-    queryFn: async () => {
-      await simulateAsync(todos);
-      console.log("i ran");
-      return todos;
-    },
-    queryKey: ["todos"],
-  });
-
-  const add = useMutation({
-    mutationFn: async (text: string) => {
-      await simulateAsync("a");
-      todoCounter++;
-      todos.push({ id: todoCounter, text } as { id: number; text: string });
-    },
-    mutationKey: ["todos"],
-    // onSuccess: (_data, variables) => {
-    //   queryClient.setQueryData(["todos"], (oldCache) => {
-    //     return [...oldCache, { id: todoCounter, text: variables }];
-    //   });
-    // },
-  });
-
-  const remove = useMutation({
-    mutationFn: async (id: string) => {
-      await simulateAsync("a");
-    },
-    onSuccess: (_data, variables) => {
-      queryClient.setQueryData(["todos"], (oldCache) => {
-        return oldCache.filter(
-          (todo: { id: number }) => todo.id !== Number(variables)
-        );
-      });
-    },
-    mutationKey: ["todos"],
-  });
-
-  return (
-    <div>
-      {query.data ? JSON.stringify(query.data) : "nothing"}
-      <br />
-      <br />
-      <input value={state} onChange={(w) => setState(w.target.value)} />
-      <button onClick={() => add.mutate(state)}>add</button>
-      <br />
-      <input value={num} onChange={(w) => setNum(w.target.value)} />
-      <button onClick={() => remove.mutate(num)}>remove</button>
-    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter basename="custom/basename/path">
+      <App />
+    </BrowserRouter>
+    <BrowserRouter basename="other/basename/path">
+      <Routes>
+        <Route path="/">
+          <div>root</div>
+        </Route>
+        <Route path="/about">
+          <div>about</div>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
